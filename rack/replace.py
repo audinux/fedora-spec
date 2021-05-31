a0ad9a04f2a8f07cc55e746294fb00d39a0b66da
Yann Collette <ycollette.nospam@free.fr>
2021-05-31 22:41:44 +0200

    improve vcvrack template replace spec script

--- rack/replace.py
+++ rack/replace.py
@@ -11,6 +11,88 @@ import os
 path_to_library_git = 'library'
 path_to_spec_files = 'spec'
 
+static_values = {
+    "ArableInstruments": {
+        "sourceUrl": r"https://github.com/adbrant/ArableInstruments",
+        "source1": "ArableInstruments.tar.gz",
+    },
+    "AudibleInstruments": {
+        "source1": "AudibleInstruments.tar.gz",
+    },
+    "AuraAudio": {
+        "sourceUrl": r"https://github.com/emurray2/auraaudio-vcv-rack",
+        "source1": r"https://github.com/emurray2/auraaudio-vcv-rack/archive/%{commit0}.tar.gz#/%{name}-%{shortcommit0}.tar.gz",
+    },
+    "BaconMusic": {
+        "source1": "BaconPlugs.tar.gz",
+    },
+    "CharredDesert": {
+        "source1": "CharredDesert.tar.gz",
+    },
+    "DrumKit": {
+        "source1": "DrumKit.tar.gz",
+    },
+    "ErraticInstruments": {
+        "source1": "Erratic.tar.gz",
+    },
+    "HamptonHarmonics": {
+        "source1": r"https://gitlab.com/hampton-harmonics/hampton-harmonics-modules/-/archive/%{commit0}/%{name}-%{shortcommit0}.tar.gz",
+    },
+    "HolonicSystems-Free": {
+        "vcvrackcxxflags": "-include cstdio ",
+    },
+    "ImpromptuModular": {
+        "vcvrackcxxflags": "-include limits ",
+    },
+    "JE": {
+        "source1": "VCVRack-plugin-JE.tar.gz",
+        "vcvrackcxxflags": "-include limits ",
+    },
+    "ODDSound_MTS_ESP": {
+        "sourceUrl": r"https://oddsound.com",
+        "source1": r"https://github.com/ODDSound/MTS-ESP-VCVRack/archive/%{commit0}.tar.gz#/%{name}-%{shortcommit0}.tar.gz",
+    },
+    "ParableInstruments": {
+        "source1": "ArableInstruments.tar.gz",
+    },
+    "SA-Seasons": {
+        "sourceUrl": r"https://github.com/SpektroAudio/Seasons",
+        "source1": r"https://github.com/SpektroAudio/Seasons/archive/%{commit0}.tar.gz#/%{name}-%{shortcommit0}.tar.gz",
+    },
+    "SonusModular": {
+        "sourceUrl": r"https://gitlab.com/sonusdept/sonusmodular",
+        "source1": r"https://gitlab.com/sonusdept/sonusmodular/-/archive/%{commit0}/%{name}-%{shortcommit0}.tar.gz",
+    },
+    "Southpole": {
+        "source1": "southpole-vcvrack.tar.gz",
+    },
+    "SurgeRack": {
+        "source1": "surge-rack.tar.gz",
+    },
+    "TheXOR": {
+        "vcvrackcxxflags": "-include limits ",
+    },
+    "moDllz": {
+        "source1": r"https://github.com/dllmusic/moDllz/archive/%{commit0}.tar.gz#/%{name}-%{shortcommit0}.tar.gz",
+    },
+    "luckyxxl": {
+        "vcvrackcxxflags": "-include limits ",
+    },
+    "modular80": {
+        "source1": "modular80.tar.gz",
+    },
+    "rcm": {
+        "vcvrackcxxflags": "-include limits ",
+    },
+    "unless_modules": {
+        "sourceUrl": r"http://gitlab.com/unlessgames/unless_modules",
+        "source1": r"http://gitlab.com/unlessgames/unless_modules/-/archive/%{commit0}/%{name}-%{shortcommit0}.tar.gz",
+    },
+    "voxglitch": {
+        "vcvrackcxxflags": "-include algorithm -include limits ",
+    },
+}
+
 # this script must be started a level before the 'library' rack repo
 # A directory 'spec' with a file 'template.spec' containing the tag SLUGNAME, VERSION, COMMITID and SOURCEURL must be created
 
@@ -44,13 +126,25 @@ def proceed(json_file):
         if 'license' in conf_rack and conf_rack['license'] == 'PROPRIETARY':
             print('Proprietary license\n')
             return
-        
+
         slug_name   = conf_rack['slug']
         version     = conf_rack['version']
         if 'sourceUrl' in conf_rack:
-            sourceurl = conf_rack['sourceUrl'].replace('.git','') # remove the trailing '.git'
+            if slug_name in static_values and 'sourceUrl' in static_values[slug_name]:
+                sourceurl = static_values[slug_name]['sourceUrl']
+            else:
+                sourceurl = conf_rack['sourceUrl'].replace('.git','') # remove the trailing '.git'
         else:
             sourceurl = ''
+        
+        source1     = r'{}/archive/%{{commit0}}.tar.gz#/%{{name}}-%{{shortcommit0}}.tar.gz'.format(sourceurl)
+        if slug_name in static_values and 'source1' in static_values[slug_name]:
+            source1 = static_values[slug_name]['source1']
+
+        vcvrackcxxflags = ''
+        if slug_name in static_values and 'vcvrackcxxflags' in static_values[slug_name]:
+            vcvrackcxxflags = static_values[slug_name]['vcvrackcxxflags']
+
         description = ''
         if 'modules' in conf_rack and 'description' in conf_rack['modules'][0]:
             description = conf_rack['modules'][0]['description']
@@ -68,6 +162,7 @@ def proceed(json_file):
         print('SLUGNAME    -> %s\n' % slug_name)
         print('VERSION     -> %s\n' % version)
         print('SOURCEURL   -> %s\n' % sourceurl)
+        print('SOURCE1     -> %s\n' % source1)
         print('COMMITID    -> %s\n' % commit_id)
         print('DESCRIPTION -> %s\n' % description)
         
@@ -89,6 +184,10 @@ def proceed(json_file):
                     print(line.replace('COMMITID',  commit_id), end='')
                 elif 'SOURCEURL' in line:
                     print(line.replace('SOURCEURL', sourceurl), end='')
+                elif 'SOURCE1' in line:
+                    print(line.replace('SOURCE1', source1), end='')
+                elif 'VCVRACKCXXFLAGS' in line:
+                    print(line.replace('VCVRACKCXXFLAGS', vcvrackcxxflags), end='')
                 elif 'DESCRIPTION' in line:
                     if description:
                         print(line.replace('DESCRIPTION', description), end='')
--- rack/template.spec
+++ rack/template.spec
@@ -21,7 +21,7 @@ URL:     SOURCEURL
 # ./rack-source.sh v1.1.6
 
 Source0: Rack.tar.gz
-Source1: SOURCEURL/archive/%{commit0}.tar.gz#/%{name}-%{shortcommit0}.tar.gz
+Source1: SOURCE1
 Source2: JSONFILE
 
 BuildRequires: gcc gcc-c++
@@ -56,7 +56,7 @@ sed -i -e "s/-march=nocona//g" compile.mk
 sed -i -e "s/-O3/-O2/g" compile.mk
 
 # %{build_cxxflags}
-echo "CXXFLAGS += -I$CURRENT_PATH/include -I$CURRENT_PATH/dep/include -I$CURRENT_PATH/dep/nanovg/src -I$CURRENT_PATH/dep/nanovg/example -I$CURRENT_PATH/dep/nanosvg/src -I/usr/include/rtmidi -I$CURRENT_PATH/dep/oui-blendish -I$CURRENT_PATH/dep/osdialog -I$CURRENT_PATH/dep/jpommier-pffft-29e4f76ac53b -I$CURRENT_PATH/dep/include  -I$CURRENT_PATH/dep/rtaudio" >> compile.mk
+echo "CXXFLAGS += VCVRACKCXXFLAGS-I$CURRENT_PATH/include -I$CURRENT_PATH/dep/include -I$CURRENT_PATH/dep/nanovg/src -I$CURRENT_PATH/dep/nanovg/example -I$CURRENT_PATH/dep/nanosvg/src -I/usr/include/rtmidi -I$CURRENT_PATH/dep/oui-blendish -I$CURRENT_PATH/dep/osdialog -I$CURRENT_PATH/dep/jpommier-pffft-29e4f76ac53b -I$CURRENT_PATH/dep/include  -I$CURRENT_PATH/dep/rtaudio" >> compile.mk
 
 sed -i -e "s/-Wl,-Bstatic//g" Makefile
 sed -i -e "s/-lglfw3/dep\/lib\/libglfw3.a/g" Makefile
