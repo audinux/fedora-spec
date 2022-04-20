5a815b4a8a895135f77f0adb1250bdc09b0e251b
Yann Collette <ycollette.nospam@free.fr>
2022-04-20 22:34:10 +0200

    cleanup

--- kxstudio/carla-change-lib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FILES="data/carla-jack-multi data/carla-rack data/carla-jack-single data/carla-single data/carla-single data/carla-patchbay data/carla-control data/carla-settings data/carla-database data/carla source/frontend/carla_skin.py source/frontend/carla_widgets.py source/frontend/carla_shared.py"
-
-for Files in $FILES
-do
-    echo "Change path from lib to lib64"
-    sed -i -e "s/\/lib\//\/lib64\//g" $Files
-done
--- kxstudio/carla-change-py.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FILES=`find . -name "*.py" -exec grep -l "env python$" {} \;`
-
-for Files in $FILES
-do
-    echo "Change python$ to python2$"
-    sed -i -e "s/env python$/env python2/g" $Files
-done
