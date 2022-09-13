bd5a6d4620ab6ea25d6545ab0663a264375d05c4
Yann COLLETTE <ycollette.nospam@free.fr>
2022-09-13 21:15:47 +0200

    update for f37 preparation

--- scripts/list-packages.sh
+++ scripts/list-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RELEASE=35
+RELEASE=36
 REPOSITORY=audinux
 
 dnf repoquery --release=$RELEASE --repoid=copr:copr.fedorainfracloud.org:ycollet:$REPOSITORY | grep src | sed -e "s/\.fc$RELEASE.src//g" | sort | uniq > packages-$RELEASE.txt
