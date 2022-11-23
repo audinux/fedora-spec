2e80c5921b432742f283d4ca5f0ee367d349eb46
Yann Collette <ycollette.nospam@free.fr>
2022-11-23 20:48:02 +0100

    update specs

--- smartguitar/source-guitarml.sh
+++ smartguitar/source-guitarml.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Usage: ./source-smartguitarpro.sh <tag>
-#        ./source-smartguitarpro.sh v1.3
+# Usage: ./source-guitarml.sh <project> <tag>
+#        ./source-guitarml.sh SmartGuitarAmp v1.3
 
-git clone https://github.com/GuitarML/SmartGuitarAmp
-cd SmartGuitarAmp
-git checkout origin/release/$1
+git clone https://github.com/GuitarML/$1
+cd $1
+git checkout origin/release/$2
 git submodule init
 git submodule update
 find . -name .git -exec rm -rf {} \;
 cd ..
-tar cvfz SmartGuitarAmp.tar.gz SmartGuitarAmp/*
-rm -rf SmartGuitarAmp
+tar cvfz $1.tar.gz $1/*
+rm -rf $1
