2a51880ba6fb14110a8a09df8367e5b2d0a19428
Yann Collette <ycollette.nospam@free.fr>
2022-12-07 21:03:37 +0100

    update to 2.2.1

--- rack/rack-source-v2.sh
+++ rack/rack-source-v2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # ./rack-source.sh <tag>
-# ./rack-source.sh v2.2.0
+# ./rack-source.sh v2.2.1
 
 git clone https://github.com/VCVRack/Rack.git Rack
 cd Rack
 git checkout $1
-git submodule updata --init --recursive
+git submodule update --init --recursive
 find . -name ".git" -exec rm -rf {} \;
 cd ..
 tar cvfz Rack.tar.gz Rack/*
