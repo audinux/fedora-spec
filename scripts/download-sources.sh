caacc05e35e03e72a2ec1408b966889c4aea3eb4
Yann Collette <ycollette.nospam@free.fr>
2023-03-15 19:06:47 +0100

    update scripts

--- scripts/download-sources.sh
+++ scripts/download-sources.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 RELEASEVER=`cat /etc/fedora-release | cut -d' ' -f3`
+TMPDIR="tmp"
+
+mkdir -p $TMPDIR
+cd $TMPDIR
 
 for Files in `dnf --releasever $RELEASEVER list --available | grep ycollet | grep src | cut -d" " -f1 | sed -e "s/\.src//g"`
 do
     echo "Downloading $Files SRPMS file"
     dnf --releasever $RELEASEVER download --source $Files > /dev/null
 done
+
+cd ..
--- scripts/list-packages.sh
+++ scripts/list-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RELEASE=36
+RELEASE=37
 REPOSITORY=audinux
 
 dnf repoquery --release=$RELEASE --repoid=copr:copr.fedorainfracloud.org:ycollet:$REPOSITORY | grep src | sed -e "s/\.fc$RELEASE.src//g" | sort | uniq > packages-$RELEASE.txt
