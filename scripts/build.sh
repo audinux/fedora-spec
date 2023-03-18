1d10781d09a42094c6c49c47d3bec58f6baad9a6
Yann Collette <ycollette.nospam@free.fr>
2023-03-18 09:56:03 +0100

    add f38

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@
 # Build spec for all the repositories and all the chroots
 
 File=$1
-copr-cli build --chroot fedora-36-x86_64 --chroot fedora-37-x86_64 --chroot fedora-37-aarch64 audinux $File
+copr-cli build --chroot fedora-36-x86_64 --chroot fedora-37-x86_64 --chroot fedora-37-aarch64 --chroot fedora-38-x86_64 --chroot fedora-38-aarch64 audinux $File
