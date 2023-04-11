57abd4062a01cf8653f089f9428729f272720550
Yann Collette <ycollette.nospam@free.fr>
2023-04-11 10:54:32 +0200

    update repo check

--- scripts/repo_check.sh
+++ scripts/repo_check.sh
@@ -837,7 +837,8 @@ https://github.com/ryukau/VSTPlugins
 https://github.com/pd3v/line
 https://github.com/kosua20/MIDIVisualizer
 https://github.com/mikeoliphant/neural-amp-modeler-lv2
-https://github.com/AidaDSP/AIDA-X"
+https://github.com/AidaDSP/AIDA-X
+https://github.com/tote-bag-labs/valentine"
 
 # These ones have disappeared
 # https://github.com/OpenMusicKontrollers/eteroj.lv2
