fa484f01a1e29807681caeaefb383ac010a2a163
Yann Collette <ycollette.nospam@free.fr>
2023-09-02 16:05:39 +0200

    update to 0.6.3

--- bambootracker/bambootracker_source.sh
+++ bambootracker/bambootracker_source.sh
@@ -6,8 +6,7 @@
 git clone https://github.com/rerrahkr/BambooTracker
 cd BambooTracker
 git checkout $1
-git submodule init
-git submodule update
+git submodule update --init --recursive --progress
 find . -name .git -exec rm -rf {} \;
 cd ..
 tar cvfz BambooTracker.tar.gz BambooTracker/*
