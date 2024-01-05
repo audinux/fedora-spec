abefb2159a2f32088a1d6b8529af87a636eab1f1
Yann Collette <ycollette@adobe.com>
2024-01-05 17:18:02 +0100

    update source script

--- lives/lives-sources.sh
+++ lives/lives-sources.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Usage: ./lives-sources.sh <TAG>
-# ./lives-sources.sh master
+#        ./lives-sources.sh master
 
-git clone --recursive https://github.com/salsaman/LiVES
+git clone https://github.com/salsaman/LiVES
 cd LiVES
 git checkout $1
-git submodule update --init --recursive
+git submodule update --init --recursive --progress
 find . -name .git -exec rm -rf {} \;
 cd ..
 tar cvfz LiVES.tar.gz LiVES/*
