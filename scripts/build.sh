c4203241703de5aa877c82c290f9ce9b4cbd3c44
Yann Collette <ycollette.nospam@free.fr>
2024-05-27 21:19:53 +0200

    remove fc38

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@
 # Build spec for all the repositories and all the chroots
 
 File=$1
-copr-cli build --chroot fedora-38-x86_64 --chroot fedora-38-aarch64 --chroot fedora-39-x86_64 --chroot fedora-39-aarch64 --chroot fedora-40-x86_64 --chroot fedora-40-aarch64 audinux --chroot fedora-rawhide-x86_64 --chroot fedora-rawhide-aarch64 $File
+copr-cli build --chroot fedora-39-x86_64 --chroot fedora-39-aarch64 --chroot fedora-40-x86_64 --chroot fedora-40-aarch64 audinux --chroot fedora-rawhide-x86_64 --chroot fedora-rawhide-aarch64 $File
