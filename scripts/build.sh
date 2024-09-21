27ed00aa466d10468cf665ac031e5d8980d21bb0
Yann Collette <ycollette.nospam@free.fr>
2024-09-21 11:01:31 +0200

    add f41

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,14 @@
 # Build spec for all the repositories and all the chroots
 
 File=$1
-copr-cli build --chroot fedora-39-x86_64 --chroot fedora-39-aarch64 --chroot fedora-40-x86_64 --chroot fedora-40-aarch64 audinux --chroot fedora-rawhide-x86_64 --chroot fedora-rawhide-aarch64 $File
+copr-cli build \
+	 --chroot fedora-39-x86_64 \
+	 --chroot fedora-39-aarch64 \
+	 --chroot fedora-40-x86_64 \
+	 --chroot fedora-40-aarch64 \
+	 --chroot fedora-41-x86_64 \
+	 --chroot fedora-41-aarch64 \
+	 --chroot fedora-rawhide-x86_64 \
+	 --chroot fedora-rawhide-aarch64 \
+	 audinux \
+	 $File
