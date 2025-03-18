dabb3f258fe8dc447e9395f6784b5db591a8a039
Yann Collette <ycollette.nospam@free.fr>
2025-03-18 21:51:02 +0100

    add fc42 beta

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,8 @@ copr-cli build \
 	 --chroot fedora-40-aarch64 \
 	 --chroot fedora-41-x86_64 \
 	 --chroot fedora-41-aarch64 \
+	 --chroot fedora-42-x86_64 \
+	 --chroot fedora-42-aarch64 \
 	 --chroot fedora-rawhide-x86_64 \
 	 --chroot fedora-rawhide-aarch64 \
 	 audinux \
--- scripts/build_devel.sh
+++ scripts/build_devel.sh
@@ -3,4 +3,4 @@
 # Build spec for all the repositories and all the chroots
 
 File=$1
-copr-cli build --chroot fedora-41-x86_64 --chroot fedora-41-aarch64 audinux $File
+copr-cli build --chroot fedora-42-x86_64 --chroot fedora-42-aarch64 audinux $File
