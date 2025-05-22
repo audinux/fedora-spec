612c8103a4e8bfa66250558b9aa4e551fd9b1fba
Yann Collette <ycollette.nospam@free.fr>
2025-05-22 12:44:45 +0200

    remove f40

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,6 @@
 
 File=$1
 copr-cli build \
-	 --chroot fedora-40-x86_64 \
-	 --chroot fedora-40-aarch64 \
 	 --chroot fedora-41-x86_64 \
 	 --chroot fedora-41-aarch64 \
 	 --chroot fedora-42-x86_64 \
