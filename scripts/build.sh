ea311f68ef0e2869813b2be29e857dfaede84a40
Yann Collette <ycollette.nospam@free.fr>
2025-09-17 06:58:27 +0200

    update build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,8 @@ copr-cli build \
 	 --chroot fedora-41-aarch64 \
 	 --chroot fedora-42-x86_64 \
 	 --chroot fedora-42-aarch64 \
+	 --chroot fedora-43-x86_64 \
+	 --chroot fedora-43-aarch64 \
 	 --chroot fedora-rawhide-x86_64 \
 	 --chroot fedora-rawhide-aarch64 \
 	 audinux \
--- scripts/build_beta.sh
+++ scripts/build_beta.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# Build spec for all the repositories and all the chroots
-
 File=$1
-copr-cli build --chroot fedora-rawhide-x86_64 --chroot fedora-rawhide-aarch64 audinux $File
+copr-cli build --chroot fedora-43-x86_64 --chroot fedora-43-aarch64 audinux $File
