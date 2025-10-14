aba4b5a63089024dee4caa62fd38724ddb85304c
Yann Collette <ycollette.nospam@free.fr>
2025-10-14 19:44:05 +0200

    add a RELEASE variable

--- scripts/check_all_package_are_build.sh
+++ scripts/check_all_package_are_build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 LIST=`find .. -name "*.spec" -exec grep "Name:" {} \; | sed -e "s/Name:[ ]*//g" | sort | uniq`
+RELEASE=43
 
 for Files in $LIST
 do
     echo "Processing spec for $Files"
-    RESULT=`LANG=C dnf search $Files 2>&1 > /dev/null`
+    RESULT=`LANG=C dnf --releasever=$RELEASE search $Files 2>&1 > /dev/null`
     RESULT=`echo $RESULT | grep "No matches found"`
     if [ ! -z "$RESULT" ]; then
        echo "Package $Files is missing"
