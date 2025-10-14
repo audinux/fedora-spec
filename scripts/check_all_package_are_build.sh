580c8c634b08e503f6ddb883f6cbc029d7ffb705
Yann Collette <ycollette.nospam@free.fr>
2025-10-14 22:55:12 +0200

    improve script

--- scripts/check_all_package_are_build.sh
+++ scripts/check_all_package_are_build.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-LIST=`find .. -name "*.spec" -exec grep "Name:" {} \; | sed -e "s/Name:[ ]*//g" | sort | uniq`
+# ./check_all_packages_are_built.sh [release]
+LIST=`find .. -name "*.spec" | sort | uniq`
+FN='missing_packages.txt'
 RELEASE=43
+if [[ $# -eq 1 ]]; then
+    RELEASE=$1
+fi
 
+echo "Updating database for F$RELEASE"
+dnf --releasever=$RELEASE --refresh search emacs 2>&1 > /dev/null
+
+echo "" > $FN
 for Files in $LIST
 do
-    echo "Processing spec for $Files"
-    RESULT=`LANG=C dnf --releasever=$RELEASE search $Files 2>&1 > /dev/null`
+    NAME=`grep "Name:" $Files | sed -e "s/Name:[ ]*//g"`
+    STATUS=`grep "# Status:" $Files | sed -e "s/# Status:[ ]*//g"`
+
+    echo "Processing spec file $Files for package name $NAME (status: $STATUS)"
+
+    RESULT=`LANG=C dnf --releasever=$RELEASE search $NAME 2>&1`
     RESULT=`echo $RESULT | grep "No matches found"`
     if [ ! -z "$RESULT" ]; then
-       echo "Package $Files is missing"
+	echo "Package $NAME is missing"
+	echo "Package $NAME from file $Files is missing (status: $STATUS)" >> $FN
     fi
 done
