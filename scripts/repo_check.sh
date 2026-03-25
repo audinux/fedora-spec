df6a452890880457bb811b9bcac5e8e396446e77
Yann Collette <yc@example.com>
2026-03-25 07:06:30 +0100

    update

--- scripts/repo_check.sh
+++ scripts/repo_check.sh
@@ -1118,7 +1118,8 @@ https://github.com/babelouest/yder
 https://github.com/unclechu/lv2-stereo-mixer
 https://github.com/hal0zer0/openwurli
 https://github.com/maolan/maolan
-https://github.com/tiagolr/sirial"
+https://github.com/tiagolr/sirial
+https://github.com/kayrockscreenprinting/ultramaster_kr106"
 
 # Missing repositories:
 # https://github.com/giogramegna/mantra-master
