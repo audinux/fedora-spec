2c63d62cdad92c01c8d9dd6a3a6b9a56cd5c01c8
Yann Collette <yc@example.com>
2026-04-28 23:12:47 +0200

    add missing script

---
+++ protoplug/protoplug-source.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Usage: ./protoplug-sources.sh <TAG>
+#        ./protoplug-sources.sh fixes
+
+git clone https://github.com/ycollet/protoplug/
+cd protoplug
+git checkout $1
+if [ $? == 1 ]; then
+    echo "Wrong branch / tag name: $1"
+    exit 1
+fi
+git submodule update --init --recursive --progress
+find . -name .git -exec rm -rf {} \;
+cd ..
+tar cvfz protoplug.tar.gz protoplug/*
+rm -rf protoplug
