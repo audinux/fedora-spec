28b7a0ddcbebd396db8f7b9480e1ee8b80528423
Yann Collette <yc@example.com>
2026-05-15 08:17:56 +0200

    update templates

---
+++ templates/orga-source.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Usage: ./brummer10-source.sh <project> <tag>
+#        ./brummer10-source.sh xtuner v1.0
+
+git clone --depth=1 https://github.com/brummer10/$1
+cd $1
+git checkout $2
+if [ $? == 1 ]; then
+    echo "Wrong branch / tag name: $2"
+    exit 1
+fi
+git submodule update --depth=1 --init --recursive --progress
+find . -name .git -exec rm -rf {} \;
+cd ..
+tar cvfz $1.tar.gz $1/*
+rm -rf $1
---
+++ templates/repo-source.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Usage: ./fire-source.sh <TAG>
+#        ./fire-source.sh v0.9.8
+
+git clone --depth=1 https://github.com/jerryuhoo/Fire/
+cd Fire
+git checkout $1
+if [ $? == 1 ]; then
+    echo "Wrong branch / tag name: $1"
+    exit 1
+fi
+git submodule update --depth=1 --init --recursive --progress
+find . -name .git -exec rm -rf {} \;
+cd ..
+tar cvfz Fire.tar.gz Fire/*
+rm -rf Fire
